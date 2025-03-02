3957ec511b95d9fe696bfd10b825cf4468237681
kabir542547301148 <mdkabirmia416454@gmail.com>
2025-03-02 16:55:09 +0600

    dev board ui with js dom manipulation

--- script/script.js
+++ script/script.js
@@ -1,21 +1,33 @@
 const completeButtons= document.querySelectorAll(".complete-btn");
-const currentDateTime = new Date();
-const formattedDateTime = currentDateTime.toLocaleString();
-console.log(formattedDateTime);
 for(let i = 0; i < completeButtons.length; i++){
     const completeBtn= completeButtons[i];
     completeBtn.addEventListener("click", function(event){
         completeBtn.setAttribute("disabled", true);
-        alert("Board Updated Successful...");
+        alert("Board Updated Successfully.....");
+
         const completeTask= completeBtn.getAttribute("disabled") === "true" ? 1 : 0;
+
         const CurrentTask= parseInt(document.getElementById("task-assigned").innerText);
-        const TotalCompletejob= parseInt(document.getElementById("total-complete-job").innerText);
+        const TotalCompleteJob= parseInt(document.getElementById("total-complete-job").innerText);
         const remainingTask= CurrentTask - completeTask;
+
         if(remainingTask === 0){
             alert("Congrats!! You have completed all the current task");
         }
+         
+
+        let now = new Date();
+         let forMattedTime = now.toLocaleString('en-US', {
+           hour: '2-digit',
+             minute: '2-digit',
+          second: '2-digit',
+          hour12: '2-digit'
+
+          })
+
+
         document.getElementById("task-assigned").innerText= remainingTask;
-        const numOfCompleteJob= TotalCompletejob + completeTask;
+        const numOfCompleteJob= TotalCompleteJob + completeTask;
         document.getElementById("total-complete-job").innerText= numOfCompleteJob;
 
         const taskTitle= document.querySelectorAll(".task-title")[i].innerText;
@@ -24,12 +36,15 @@ for(let i = 0; i < completeButtons.length; i++){
         const div= document.createElement("div");
         div.innerHTML= `
             <div class="bg-[#F4F7FF] p-4 text-justify rounded-lg space-y-3 logs">
-                <p>You have Completed the ${taskTitle} at 12.30 p.M</p>      
+                <p>You have Completed the ${taskTitle} at ${forMattedTime} </p>      
             </div>
         `
         taskLog.appendChild(div);
-    });
+    });  
 }
+
+
+    
 document.getElementById("clear-logs").addEventListener("click", function(event){
     const logs= document.getElementsByClassName("logs");
     for(let i= 0; i < logs.length; i++){
@@ -38,7 +53,6 @@ document.getElementById("clear-logs").addEventListener("click", function(event){
     }
 
 });
-
 
 document.getElementById('discover-new-page').addEventListener('click', function() {
     window.location.href=('main.html');
@@ -51,3 +65,9 @@ colorBtn.addEventListener('click',  function(){
     const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
     document.body.style.backgroundColor = randomColor;
 });
+
+
+
+
+
+
